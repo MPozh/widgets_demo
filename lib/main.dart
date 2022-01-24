242ce5642d57e1f36cc7fc86fc0f3c60bd51b3fb
MPozh <m.pozheritskaya@gmail.com>
2022-01-25 00:37:13 +0300

    Добавлен ящик

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'text_demo_screen.dart';
 import 'switches_demo_screen.dart';
 import 'pizza_screen.dart';
+import 'navigation_drawer_screen.dart';
 
 void main() {
-  runApp(const PizzaCalcScreen());
+  //runApp(const PizzaCalcScreen());
+  //runApp(const SwitchesDemoScreen ());
+  //runApp(const TextDemoScreen ());
+  runApp(const NavigationDrawerScreen ());
 }
 
 
